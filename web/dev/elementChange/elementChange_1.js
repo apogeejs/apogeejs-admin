88168489bc1f4f576c8a910f963e0a03ec5a58c2
Micello <Micello@Micello-PC.attlocal.net>
2016-04-22 15:52:29 -0700

    made some sample code for the iframe resize method

--- web/dev/elementChange/elementChange_1.js
+++ web/dev/elementChange/elementChange_1.js
@@ -8,44 +8,38 @@
 
 (function(){
   
-  function resizeListener(e){
-    var win = e.target || e.srcElement;
-    if (win.__resizeRAF__) window.cancelAnimationFrame(win.__resizeRAF__);
-    win.__resizeRAF__ = window.requestAnimationFrame(function(){
-      var trigger = win.__resizeTrigger__;
-      trigger.__resizeListeners__.forEach(function(fn){
-        fn.call(trigger, e);
-      });
-    });
-  }
   
-  function objectLoad(e){
-    this.contentDocument.defaultView.__resizeTrigger__ = this.__resizeElement__;
-    this.contentDocument.defaultView.addEventListener('resize', resizeListener);
-  }
+
   
-  window.addResizeListener = function(element, fn){
-    if (!element.__resizeListeners__) {
-      element.__resizeListeners__ = [];
+  window.addResizeListener = function(element, resizeCallback){
+	  var styleJson = {
+		"position":"absolute",
+        "top":"0px",
+        "left":"0px",
+		"bottom":"0px",
+        "right":"0px",
+		"overflow":"hidden",
+		"zIndex":-1
+	};
+	var dummyFrameElement = visicomp.visiui.createElement("iframe",null,styleJson);
+	
 
-		if (getComputedStyle(element).position == 'static') element.style.position = 'relative';
-		var obj = element.__resizeTrigger__ = document.createElement('object'); 
-		obj.setAttribute('style', 'display: block; position: absolute; top: 0; left: 0; height: 100%; width: 100%; overflow: hidden; pointer-events: none; z-index: -1;');
-		obj.__resizeElement__ = element;
-		obj.onload = objectLoad;
-		obj.type = 'text/html';
-		obj.data = 'about:blank';
-		element.appendChild(obj);
-
-    }
-    element.__resizeListeners__.push(fn);
+	var onLoadCallback = function() {
+		
+		var dummyFrameBody = dummyFrameElement.contentDocument.body;
+		dummyFrameBody.onresize = resizeCallback;
+		
+		//onloadCallback();
+		resizeCallback();
+	}
+	
+	//do a resize on load too
+	dummyFrameElement.onload = onLoadCallback;
+	
+	element.appendChild(dummyFrameElement);
   };
   
   window.removeResizeListener = function(element, fn){
-    element.__resizeListeners__.splice(element.__resizeListeners__.indexOf(fn), 1);
-    if (!element.__resizeListeners__.length) {
-        element.__resizeTrigger__.contentDocument.defaultView.removeEventListener('resize', resizeListener);
-        element.__resizeTrigger__ = !element.removeChild(element.__resizeTrigger__);
-    }
+	  alert("remove resize listener not implemented");
   }
 })();
