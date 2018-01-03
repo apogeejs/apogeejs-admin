94421951cea39209281e92a970b6d165b91f9bcf
sutter <sutter@intransix.com>
2018-01-02 16:52:10 -0800

    fixed the close logic, in web and electron. There were some cases there the dirty flag was not checked correctly.

--- web/apogeeapp/test-electron/main.js
+++ web/apogeeapp/test-electron/main.js
@@ -7,38 +7,57 @@ const url = require('url')
 let win
 
 function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
-  win.setMenu(null)
+    // Create the browser window.
+    win = new BrowserWindow({width: 800, height: 600})
+    win.setMenu(null)
 
-  // and load the index.html of the app.
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'apogee.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+    // and load the index.html of the app.
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'apogee.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
   
    // Open the DevTools.
-  win.webContents.openDevTools()
+    win.webContents.openDevTools()
+    
+    
   
-  win.on('close',(e) => {
-	  const {dialog} = require('electron');
-	  var result = dialog.showMessageBox({
-		message: "Are you sure you want to exit?",
-		buttons: ["Exit","Stay"]
-	  });
-	  if(result === 1) {
-	    e.preventDefault();
-	  }
-  })
+    win.on('close',(e) => {
+        const {dialog} = require('electron');
+ 
+        var isDirtyPromise = win.webContents.executeJavaScript("app.getWorkspaceIsDirty()");
+        isDirtyPromise.then( (isDirty) => {
+            var doClose;
+            if(isDirty) {
+                var resultIndex = dialog.showMessageBox({
+                    message: "There is unsaved data. Are you sure you want to exit?",
+                    buttons: ["Exit","Stay"]
+                });
+                doClose = (resultIndex == 0);
+            }
+            else {
+                doClose = true;
+            }
+            
+            console.log("Do close result: " + doClose);
+            
+            if(doClose) {
+                win.destroy();
+            }
+        });
+        
+        //we won't close here - we will use promise result above
+        e.preventDefault();
+    });
 
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null
-  })
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null
+    })
 }
 
 // This method will be called when Electron has finished
@@ -48,19 +67,19 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
-  }
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+        createWindow()
+    }
 })
 
 // In this file you can include the rest of your app's specific main process
