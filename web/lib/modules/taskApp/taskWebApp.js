2411dc9b70ba97039a6eac2328900d7f23bf5b03
sutter <sutter@intransix.com>
2018-01-08 15:33:32 -0800

    updates for web app and task web app

--- web/lib/modules/taskApp/taskWebApp.js
+++ web/lib/modules/taskApp/taskWebApp.js
@@ -9,14 +9,17 @@ taskWebApp.initTaskApp = function(workspaceUrl,configData,containerElementId,ini
     var _configData = configData;
     var _initialComponentName = initialComponentName;
     var _activeDisplayFrame = null;
-    
+    var _titleElementHeight = 0;
+    var _titleTotalHeight = 0;
 
     function onWorkspaceLoad() {
-
         //load the UI controls
         for(var fullName in _configData) {
             initDisplay(fullName,_configData[fullName]);
         }
+        
+        //remove loading label
+        apogeeapp.ui.removeAllChildren(_displayPanel);
 
         //set the initial component
         setActiveComponent(_initialComponentName);
@@ -30,18 +33,31 @@ taskWebApp.initTaskApp = function(workspaceUrl,configData,containerElementId,ini
 
         //create wrapper dom Element
         var wrapperElement = document.createElement("div");
-        wrapperElement.className = "wrapperElementClass";
+        wrapperElement.className = "taskApp_wrapperElementClass";
 
-        var titleElement = document.createElement("h3");
-        titleElement.innerHTML = configEntry.title;
+        var titleElement = getTitleElement(configEntry.title);
+        titleElement.style.height = _titleElementHeight + "px";
         wrapperElement.appendChild(titleElement);
 
         var frameHolder = document.createElement("div");
-        frameHolder.className = "frameHolderClass";
-        frameHolder.appendChild(displayFrame.getElement())
+        frameHolder.className = "taskApp_frameHolderClass";
+        frameHolder.appendChild(displayFrame.getElement());
+        frameHolder.style.top = _titleTotalHeight + "px";
         wrapperElement.appendChild(frameHolder);
 
         configEntry.panel = wrapperElement;             
+    }
+    
+    function getTitleElement(name) {
+        var titleElement = document.createElement("h2");
+        titleElement.innerHTML = name;
+        titleElement.className = "taskApp_titleElement";
+        return titleElement;
+    }
+    
+    function getActualHeight(element) {
+        var styles = getComputedStyle(element);
+        return Math.ceil(parseFloat(styles.marginTop) + element.offsetHeight + parseFloat(styles['marginBottom']));
     }
 
 
@@ -78,6 +94,12 @@ taskWebApp.initTaskApp = function(workspaceUrl,configData,containerElementId,ini
     //==================
     // Init body
     //==================
+    
+    //create a loading page - this is to measure the title height
+    var loadingElement = getTitleElement("loading...");
+    _displayPanel.appendChild(loadingElement);
+    _titleElementHeight = loadingElement.clientHeight;
+    _titleTotalHeight = getActualHeight(loadingElement);
     
     //set some globals
      __globals__.__WEB_APP_MAKE_ACTIVE_FUNCTION__ = setActiveComponent;
