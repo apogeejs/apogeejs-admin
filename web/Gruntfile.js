a6ae04e81602fc58aa890adf0f2b972755d960b3
sutter-dave <sutter@intransix.com>
2018-06-18 17:53:52 -0700

    updates from release 0.8.6

--- web/Gruntfile.js
+++ web/Gruntfile.js
@@ -276,7 +276,7 @@ module.exports = function(grunt) {
 
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-uglify-es');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-concat-css');
   grunt.loadNpmTasks('grunt-contrib-copy');
