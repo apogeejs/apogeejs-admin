24bb8861dd970a9589cb841a37d7e3273daa924d
sutter-dave <sutter@intransix.com>
2019-04-06 15:33:51 -0700

    added some testing for ProseMirror

---
+++ web/dev/editors/prosemirror/basicExample/index.js
@@ -0,0 +1,20 @@
+const {EditorState} = require("prosemirror-state")
+const {EditorView} = require("prosemirror-view")
+const {Schema, DOMParser} = require("prosemirror-model")
+const {schema} = require("prosemirror-schema-basic")
+const {addListNodes} = require("prosemirror-schema-list")
+const {exampleSetup} = require("prosemirror-example-setup")
+
+// Mix the nodes from prosemirror-schema-list into the basic schema to
+// create a schema with list support.
+const mySchema = new Schema({
+  nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
+  marks: schema.spec.marks
+})
+
+window.view = new EditorView(document.querySelector("#editor"), {
+  state: EditorState.create({
+    doc: DOMParser.fromSchema(mySchema).parse(document.querySelector("#content")),
+    plugins: exampleSetup({schema: mySchema})
+  })
+})
