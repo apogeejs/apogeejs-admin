cdcd4fd3bf737a63a371a3215bd43c178e4aff9f
sutter-dave <sutter@intransix.com>
2020-06-17 12:28:15 -0700

    get rid of a debug msg

--- web/run/router.js
+++ web/run/router.js
@@ -1,12 +1,10 @@
 var parser = require('url');
 var {processFileRequest} = require("./filehandler");
-var {mapPath} = require("./pathmapper");
+//var {mapPath} = require("./pathmapper");
 
 module.exports.route = function(request,response) {
     var url = parser.parse(request.url,true);
     var inPath = url.pathname.toString();
-
-    console.log(inPath);
 
     //this is if we want to remap path names
     //var outPath = mapPath(inPath);
