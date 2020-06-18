f5374a9a1fe2a7c3e3211041766a510f2a13cb70
sutter-dave <sutter@intransix.com>
2020-06-18 09:58:47 -0700

    updated server to run with or without path remap; added launch configurations for the two cases

--- web/run/router.js
+++ web/run/router.js
@@ -1,14 +1,27 @@
 var parser = require('url');
 var {processFileRequest} = require("./filehandler");
-//var {mapPath} = require("./pathmapper");
+var {mapPath} = require("./pathmapper");
+
+var _doRemap_ = false;
+
+//If this is called with the arg "true", path remapping will be done.
+//be default path remapping is not done.
+module.exports.setRemap = function(doRemap) {
+    _doRemap_ = doRemap;
+}
 
 module.exports.route = function(request,response) {
     var url = parser.parse(request.url,true);
     var inPath = url.pathname.toString();
 
     //this is if we want to remap path names
-    //var outPath = mapPath(inPath);
-    var outPath = inPath;
+    var outPath;
+    if(_doRemap_) {
+        outPath = mapPath(inPath);
+    }
+    else {
+        outPath = inPath;
+    }
 
     processFileRequest(outPath,response);
 }
