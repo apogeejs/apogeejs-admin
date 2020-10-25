4f472136dcb37ccd7d6e1b5bc3c0bebb48ae2ce9
sutter-dave <sutter@intransix.com>
2020-10-25 11:54:49 -0700

    release 1.2.2

---
+++ web/releases/lib/v1.2.2/nodeGlobals.js
@@ -0,0 +1,17 @@
+//some global definitions for the node environment
+global.__globals__ = global;
+global.__APOGEE_ENVIRONMENT__ = "NODE";
+
+//these function should be replaced with an implementation at the appication level
+
+/** This prints a message in the alert log */
+__globals__.apogeeLog = (msg) => console.log(message);
+
+/** This function asynchronously gives an alert the user */
+__globals__.apogeeUserAlert = (msg) => undefined;
+
+/** This function asynchornously lets the confirm or cancel an action. */
+__globals__.apogeeUserConfirm = (msg,okText,cancelText,okAction,cancelAction,defaultToOk) => defaultToOk ? okAction : cancelAction;
+
+/** This synchronous funtion returns true or false for ok or cancel. */
+__globals__.apogeeUserConfirmSynchronous = (msg,okText,cancelText,defaultToOk) => defaultToOk;
---
+++ web/releases/lib/v1.2.2/webGlobals.js
@@ -0,0 +1,17 @@
+//some global definitions for the web browser environment
+window.__globals__ = window;
+window.__APOGEE_ENVIRONMENT__ = "WEB";
+
+//these function should be replaced with an implementation at the appication level
+
+/** This prints a message in the alert log */
+__globals__.apogeeLog = (msg) => console.log(message);
+
+/** This function asynchronously gives an alert the user */
+__globals__.apogeeUserAlert = (msg) => undefined;
+
+/** This function asynchronously lets the confirm or cancel an action. */
+__globals__.apogeeUserConfirm = (msg,okText,cancelText,okAction,cancelAction,defaultToOk) => defaultToOk ? okAction : cancelAction;
+
+/** This synchronous funtion returns true or false for ok or cancel. */
+__globals__.apogeeUserConfirmSynchronous = (msg,okText,cancelText,defaultToOk) => defaultToOk;
